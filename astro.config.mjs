74c493d294292eb7ed2bb56cee47574741d38779
Noah Gentile <noah.gentile@gmail.com>
2024-04-27 12:21:06 -0700

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,24 @@
 import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
-
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
+const {
+  SANITY_DATASET,
+  SANITY_PROJECT_ID
+} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
-const { SANITY_DATASET, SANITY_PROJECT_ID } = loadEnv(
-  process.env.NODE_ENV,
-  process.cwd(),
-  ""
-);
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-
   server: {
-    port: 3333,
+    port: 3333
   },
-
-  integrations: [
-    react(),
-    sanity({
-      projectId: SANITY_PROJECT_ID,
-      dataset: SANITY_DATASET,
-
-      studioBasePath: "/studio",
-    }),
-  ],
+  integrations: [react(), sanity({
+    projectId: SANITY_PROJECT_ID,
+    dataset: SANITY_DATASET,
+    studioBasePath: "/studio"
+  })],
+  adapter: vercel()
 });
